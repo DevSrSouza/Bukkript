211e31504e074d10e293b0aded9205e1d71c180f
DevSrSouza <devsrsouza@gmail.com>
2020-09-10 20:25:12 -0300

    Chaging Bukkit dependency to Spigot

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -1,7 +1,7 @@
 object Dep {
     val kotlinBukkitAPI = KotlinBukkitAPI
 
-    val spigot = "org.bukkit:bukkit:1.8.8-R0.1-SNAPSHOT"
+    val spigot = "org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT"
     val ivy = "org.apache.ivy:ivy:2.5.0"
 
     val bstats = "org.bstats:bstats-bukkit:1.7"
